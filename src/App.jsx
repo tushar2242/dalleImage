46341791fb889da9279c28a4010658f0328823c9
Alok Sahu <tusharsahu2427@gmail.com>
2024-02-17 02:48:15 +0530

    updated 1.1 minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Let Work on that</Link>
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create new Images</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
